902d1e28e430e22ee4beba6431aa703f76cce6ec
elpuas <elpuas@gmail.com>
2018-06-12 14:59:34 -0400

    althea scripts

--- functions.php
+++ functions.php
@@ -1,16 +1,16 @@
 slype<?php
-function blackandbluefishing_enqueue_styles() {
+function althea_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'blackandbluefishing-google-fonts', 'https://fonts.googleapis.com/css?family=Fira+Mono:400|Open+Sans+Condensed:700|Roboto+Condensed', false );
+    wp_enqueue_style( 'althea-google-fonts', 'https://fonts.googleapis.com/css?family=Fira+Mono:400|Open+Sans+Condensed:700|Roboto+Condensed', false );
 
 }
-add_action( 'wp_enqueue_scripts', 'blackandbluefishing_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'althea_enqueue_styles' );
 
 
-add_action( 'wp_enqueue_scripts', 'blackandbluefishing_scripts' );
+add_action( 'wp_enqueue_scripts', 'althea_scripts' );
 
-function blackandbluefishing_scripts() {
-  wp_enqueue_script( 'blackandbluefishing-script', get_stylesheet_directory_uri() . '/bb-scripts.js', array( 'jquery' )
+function althea_scripts() {
+  wp_enqueue_script( 'althea-script', get_stylesheet_directory_uri() . '/althea-scripts.js', array( 'jquery' )
   );
 }
 
