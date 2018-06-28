a591762b54d9428b1a763d102537c4e503e365a8
elpuas <elpuas@gmail.com>
2018-06-28 19:49:12 -0400

    test map-links

--- althea-scripts.js
+++ althea-scripts.js
@@ -1,5 +1,11 @@
 jQuery(document).ready(function($){
-  
+  // Call RWD Image Maps
+  $('img[usemap]').rwdImageMaps();
+
+  $('#countryMap1').hover(function(){
+    console.log('Hola soy mapa uno');
+  });
+
  console.log("%c Made with  💖 and a lot of  ☕ by el.puas | https://elpuas.com ", "color:#fff;background:#000;");
 
 });
