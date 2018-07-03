d1319e2d1e9d085c159808df5508847b597b9cf6
elpuas <elpuas@gmail.com>
2018-07-02 20:20:46 -0400

    althea scripts

--- althea-scripts.js
+++ althea-scripts.js
@@ -1,21 +1,16 @@
 jQuery(document).ready(function($){
 
-  $(function(){
-
     if ($('#altheaMapArea').lenght > 0 ) {
       // get parent div height
       var mapHeight = $('#altheaMapArea').height();
-
       console.log(mapHeight);
 
       // Slider Hover Function
       $('#countryMap1').hover(function(){
+          
           console.log('Hola soy mapa uno');
-
-        });
+      });
     }
-
-  });
 
  console.log("%c Made with  💖 and a lot of  ☕ by el.puas | https://elpuas.com ", "color:#fff;background:#000;");
 
