0ac263727ac8b60dcbe7866135d4732cd4cf2f0e
elpuas <elpuas@gmail.com>
2018-07-02 20:28:43 -0400

    lets try this

--- althea-scripts.js
+++ althea-scripts.js
@@ -1,16 +1,17 @@
 jQuery(document).ready(function($){
 
-    if (  $('#altheaMapArea').lenght ) {
+
       // get parent div height
-      var mapHeight = $('#altheaMapArea').height();
-      console.log(mapHeight);
+      var mapHeight = $('.image-map').height();
+      var parentChild = $('#altheaAnimation');
+      console.log(mapHeight + "Que Saldra" + parentChild);
 
       // Slider Hover Function
       $('#countryMap1').hover(function(){
 
           console.log('Hola soy mapa uno');
       });
-    }
+
 
  console.log("%c Made with  💖 and a lot of  ☕ by el.puas | https://elpuas.com ", "color:#fff;background:#000;");
 
