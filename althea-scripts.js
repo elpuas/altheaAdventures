9dcd90ef0c8396ce169bc0ea2898b51cb7dacedc
elpuas <elpuas@gmail.com>
2018-07-02 20:03:34 -0400

    althea scripts map

--- althea-scripts.js
+++ althea-scripts.js
@@ -1,9 +1,16 @@
 jQuery(document).ready(function($){
 
+  var mapSlider = function(){
+        var height = $('#altheaMapArea').height();
+  }
+
+  console.log(calcHeight());
+
   $('#countryMap1').hover(function(){
     console.log('Hola soy mapa uno');
   });
 
  console.log("%c Made with  💖 and a lot of  ☕ by el.puas | https://elpuas.com ", "color:#fff;background:#000;");
+
 
 });
