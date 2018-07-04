049959f897b81b68f120f32f8990b2a2341d0e19
elpuas <elpuas@gmail.com>
2018-07-04 14:36:38 -0400

    slider scripts

--- althea-scripts.js
+++ althea-scripts.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function($){
+
     // Check if Element Exist in the DOM
     $.fn.exists = function(callback) {
         var args = [].slice.call(arguments, 1);
@@ -8,34 +9,39 @@ jQuery(document).ready(function($){
         }
       
         return this;
-      };
+    };
       
       // Usage
       $('#altheaMapArea').exists(function() {
-        console.log('Si Existo /n');
-      });
+        console.log('Si Existo');
 
-      // get parent div height
+      // Get element height
       var mapHeight = $('#altheaMapArea .et_pb_code').height();
-        $('[id="altheaSliderItem"]').css("height", mapHeight);
 
-      // Slider Hover Function
-      $('#countryMap1').onclick(function(){
-            console.log()
-      });
-      $('#countryMap2').hover(function(){
+      // Add same height to siblings
+      $('[id="altheaSliderItem"]').css("height", mapHeight);
 
-          console.log('Hola soy mapa uno');
+      // Slider onClick Function
+      $('#countryMap1').click(function(){
+            console.log('Hola Soy countryMap1');
       });
+
+      $('#countryMap2').click(function(){
+
+          console.log('Hola Soy countryMap2');
+      });
+
       $('#countryMap3').hover(function(){
 
-          console.log('Hola soy mapa uno');
+          console.log('Hola Soy countryMap3');
       });
+    });
 
 
 
-
+ // Branding
  console.log("%c Made with  💖 and a lot of  ☕ by el.puas | https://elpuas.com ", "color:#fff;background:#000;");
-
+ 
+ // End Document Ready
 
 });
